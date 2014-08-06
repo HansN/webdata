f54e90d7452cba3e2d0950e64d7592c678bbbd85
Hans Nilsson <hans@nilsson.ws>
2014-08-06 16:21:52 +0200

    initial

---
+++ src/webdata.erl
@@ -0,0 +1,18 @@
+-module(webdata).
+
+%% webdata: webdata library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
